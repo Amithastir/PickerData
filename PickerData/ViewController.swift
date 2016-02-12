9f29d87f7f36155e243e3991959e045411615913
Amit Hastir <amithastir@gmail.com>
2016-02-12 12:08:30 +0530

    PiclerData
    Sample code for picker Data

--- PickerData/ViewController.swift
+++ PickerData/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    let mood_array=["Happy","Sad","Feeling Exicted","Mood-Off","Upset","Loving","Funny","Crazy"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return mood_array.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return mood_array[row]
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var newbackground:UIColor
+        switch row{
+        case 0,2,4:
+            newbackground=UIColor.yellowColor()
+        case 1,3,5:
+            newbackground=UIColor.darkGrayColor()
+        default :
+            newbackground=UIColor(red: 200/255, green: 255/255, blue: 200/255, alpha: 1.0)
+        }
+        self.view.backgroundColor=newbackground
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
